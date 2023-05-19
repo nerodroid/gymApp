0fc18937691ba4fabe20ba7900c0c5d4d3ea9197
Niranga Sithara <nerodroid.dev@gmail.com>
2023-05-19 23:32:27 +0530

    Add cspell and navigation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  const babelEnv = api.env();
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ];
+
+  if (babelEnv === 'production') {
+    plugins.push(['transform-remove-console']);
+  }
+
+  plugins.push('react-native-reanimated/plugin');
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: plugins,
+  };
 };
